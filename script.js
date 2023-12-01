1d258f23875e9822396faa66236c3b3d10ac4661
bhaskarAntony <rooparoopa7105@gmail.com>
2023-12-01 22:11:48 +0530

    local storage is implemented

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ const themeChanger = ()=>{
     if(isLightTheme){
         themeIcon.classList.remove("fa-cloud-moon")
         themeIcon.classList.add("fa-cloud-sun")
+        
         document.body.style.background = "#fff"
         document.body.style.color="#000"
         // nav
@@ -44,4 +45,45 @@ const themeChanger = ()=>{
         topbar.style.backgroundColor = "#252525"
     }
 }
+
+//to save the data
+function saveNote(){
+    var title = document.getElementById('title').value;
+    var note = document.getElementById('content').value;
+    const mynotes = localStorage.getItem('noteData') ||'[]';
+
+    const dataArray = JSON.parse(mynotes);
+
+    dataArray.push({title, note});
+    //save note to local store
+    localStorage.setItem('noteData', JSON.stringify(dataArray));
+    alert('notes has been saved..')
+  
+    dialogOpener();
+    displayNotes();
+}
+
+const displayNotes = ()=>{
+    const container = document.getElementById('notes-container');
+    const mynotes = localStorage.getItem('noteData') ||'[]'; //json formate
+    const allNotes = JSON.parse(mynotes) //object formate
+    allNotes.forEach(element => {
+        container.innerHTML+=`
+        <div class="col-12 col-sm-12 col-md-4 col-lg-3 mb-3">
+        <div class="notes-card h-100 p-5">
+        <h5 class="text-danger fs-4">${element.title}</h5>
+        <p class="text-secondary fs-5">${element.note}</p>
+        <div class="icons d-flex align-items-center gap-3">
+        <i class="fa fa-heart"></i>
+        <i class="fa fa-edit"></i>
+        <i class="fa fa-trash"></i>
+        <i class="fa fa-share"></i>
+      </div>
+      </div>
+        </div>
+        `
+    });
+
+}
+displayNotes();
 
